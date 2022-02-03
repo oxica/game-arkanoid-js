1e0fec97a517f10589873d8c929d788d40ea001d
oxica <kskaplun@gmail.com>
2022-02-03 14:07:58 +0200

    03

--- game.js
+++ game.js
@@ -1,11 +1,41 @@
 let game = {
-  start: function () {
+  ctx: null,
+  sprites: { background: null, ball: null, platform: null },
+
+  init: function () {
     this.ctx = document.getElementById("mycanvas").getContext("2d");
-    let background = new Image();
-    background.src = "images/background.png";
+  },
+
+  preload(callback) {
+    let loaded = 0;
+    let required = Object.keys(this.sprites).length;
+    for (let key in this.sprites) {
+      this.sprites[key] = new Image();
+      this.sprites[key].src = "img/" + key + ".png";
+      this.sprites[key].addEventListener("load", () => {
+        ++loaded;
+        if (loaded >= required) {
+          callback();
+        }
+      });
+    }
+  },
+
+  run() {
     window.requestAnimationFrame(() => {
-      this.ctx.drawImage(background, 0, 0);
+      this.render();
     });
+  },
+
+  render() {
+    this.ctx.drawImage(this.sprites.background, 0, 0);
+    this.ctx.drawImage(this.sprites.ball, 0, 0);
+    this.ctx.drawImage(this.sprites.platform, 0, 0);
+  },
+
+  start: function () {
+    this.init();
+    this.preload(() =>{this.run()});
   },
 };
 
