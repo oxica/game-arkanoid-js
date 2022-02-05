1e624e9882758b26cc760a1975d17917107f3336
oxica <kskaplun@gmail.com>
2022-02-05 18:41:23 +0200

    05

--- game.js
+++ game.js
@@ -1,7 +1,10 @@
 let game = {
   ctx: null,
   platform: null,
-  sprites: { background: null, ball: null, platform: null },
+  blocks: [],
+  rows: 4,
+  cols: 8,
+  sprites: { background: null, ball: null, platform: null, block: null },
 
   init: function () {
     this.ctx = document.getElementById("mycanvas").getContext("2d");
@@ -10,15 +13,27 @@ let game = {
   preload(callback) {
     let loaded = 0;
     let required = Object.keys(this.sprites).length;
+    let onImageLoad = () => {
+      ++loaded;
+      if (loaded >= required) {
+        callback();
+      }
+    };
     for (let key in this.sprites) {
       this.sprites[key] = new Image();
       this.sprites[key].src = "img/" + key + ".png";
-      this.sprites[key].addEventListener("load", () => {
-        ++loaded;
-        if (loaded >= required) {
-          callback();
-        }
-      });
+      this.sprites[key].addEventListener("load", onImageLoad);
+    }
+  },
+
+  create() {
+    for (let row = 0; row < this.rows; row++) {
+      for (let col = 0; col < this.cols; col++){
+        this.blocks.push({
+          x: 64 * col + 65,
+          y: 24 * row + 35
+        });
+      }
     }
   },
 
@@ -42,24 +57,31 @@ let game = {
       this.ball.height
     );
     this.ctx.drawImage(this.sprites.platform, this.platform.x, this.platform.y);
+    this.renderBlocks();
   },
+
+renderBlocks(){for (let block of this.blocks) {
+        this.ctx.drawImage(this.sprites.block, block.x, block.y);
+      }},
 
   start: function () {
     this.init();
-    this.preload(() =>{this.run()});
+    this.preload(() => {
+      this.create();
+      this.run();
+    });
   },
 };
 
-
-game.ball = { 
+game.ball = {
   x: 320,
   y: 280,
   width: 22,
-  height: 22
+  height: 22,
 };
 game.platform = {
   x: 280,
-  y: 300
+  y: 300,
 };
 
 window.addEventListener("load", () => {
