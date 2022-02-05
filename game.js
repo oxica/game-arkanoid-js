83352841269855a0ed091598f0beb4584fd42203
oxica <kskaplun@gmail.com>
2022-02-05 18:19:35 +0200

    04

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 let game = {
   ctx: null,
+  platform: null,
   sprites: { background: null, ball: null, platform: null },
 
   init: function () {
@@ -29,14 +30,36 @@ let game = {
 
   render() {
     this.ctx.drawImage(this.sprites.background, 0, 0);
-    this.ctx.drawImage(this.sprites.ball, 0, 0);
-    this.ctx.drawImage(this.sprites.platform, 0, 0);
+    this.ctx.drawImage(
+      this.sprites.ball,
+      0,
+      0,
+      this.ball.width,
+      this.ball.height,
+      this.ball.x,
+      this.ball.y,
+      this.ball.width,
+      this.ball.height
+    );
+    this.ctx.drawImage(this.sprites.platform, this.platform.x, this.platform.y);
   },
 
   start: function () {
     this.init();
     this.preload(() =>{this.run()});
   },
+};
+
+
+game.ball = { 
+  x: 320,
+  y: 280,
+  width: 22,
+  height: 22
+};
+game.platform = {
+  x: 280,
+  y: 300
 };
 
 window.addEventListener("load", () => {
