2d8b20a9298ed89f54d92f2d19d27ad60c1d2da2
oxica <kskaplun@gmail.com>
2022-03-22 23:54:47 +0200

    15 обработка поражения

--- game.js
+++ game.js
@@ -5,6 +5,7 @@ const KEYS = {
 };
 
 let game = {
+  running: true,
   ctx: null,
   platform: null,
   ball: null,
@@ -85,11 +86,13 @@ let game = {
     }
   },
   run() {
-    window.requestAnimationFrame(() => {
-      this.update();
-      this.render();
-      this.run();
-    });
+    if (this.running) {
+      window.requestAnimationFrame(() => {
+        this.update();
+        this.render();
+        this.run();
+      });
+    }
   },
   render() {
     this.ctx.clearRect(0, 0, this.width, this.height);
@@ -185,7 +188,9 @@ game.ball = {
       this.y = 0;
       this.dy = this.velocity;
     } else if (ballBottom > worldBottom) {
-      console.log("game over");
+      game.running = false;
+      alert("Game over!");
+      window.location.reload();
     }
   },
 
