06fed655d7a5362e19ae87ee8906f3525ebb07e6
oxica <kskaplun@gmail.com>
2022-03-22 23:00:51 +0200

    13 отскок мяча от краев экрана

--- game.js
+++ game.js
@@ -65,10 +65,12 @@ let game = {
     }
   },
   update() {
-    this.platform.move();
-    this.ball.move();
     this.collideBlocks();
     this.collidePlatform();
+    this.ball.collideWorldBounds();
+    this.platform.move();
+    this.ball.move();
+    
   },
   collideBlocks() {
       for (let block of this.blocks) {
@@ -159,14 +161,44 @@ game.ball = {
     }
     return false;
   },
+  collideWorldBounds() {
+  let x = this.x + this.dx;
+  let y = this.y + this.dy;
+
+    let ballLeft = x;
+    let ballRight = ballLeft + this.width;
+    let ballTop = y;
+    let ballBottom = ballTop + this.height;
+
+    let worldLeft = 0;
+    let worldRight = game.width;
+    let worldTop = 0;
+    let worldBottom = game.height;
+
+    if (ballLeft < worldLeft) {
+      this.x = 0;
+      this.dx = this.velocity;
+    } else if (ballRight > worldRight) {
+      this.x = worldRight - this.width;
+      this.dx = -this.velocity;
+    } else if (ballTop < worldTop) {
+      this.y = 0;
+      this.dy = this.velocity;
+    } else if (ballBottom > worldBottom) {
+      console.log('game over');
+    }
+},
+
   bumpBlock(block) {
     this.dy *= -1;
     block.active = false;
   },
   bumpPlatform(platform) {
-    this.dy *= -1;
+    if (this.dy > 0) {
+      this.dy = -this.velocity;
     let touchX = this.x + this.width / 2;
     this.dx = this.velocity * platform.getTouchOffset(touchX);
+    }
   },
 };
 
